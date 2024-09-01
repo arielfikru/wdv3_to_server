633254ebb487e3049b2eb4bde797f80909788a36
Ariel Fikru Avicenna <71917936+arielfikru@users.noreply.github.com>
2024-09-01 15:33:27 +0700

    Update wdv3.py

--- wdv3.py
+++ wdv3.py
@@ -20,6 +20,7 @@ MODEL_REPO_MAP = {
     "vit": "SmilingWolf/wd-vit-tagger-v3",
     "swinv2": "SmilingWolf/wd-swinv2-tagger-v3",
     "convnext": "SmilingWolf/wd-convnext-tagger-v3",
+    "eva02": "SmilingWolf/wd-eva02-large-tagger-v3",
 }
 
 @flax.struct.dataclass
